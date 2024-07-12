f59e1203328b8d46d7035f9294468153459e3271
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-12 17:21:28 -0400

    Implemented a horizontal wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+    
+  const columnLength = letters.length;
+  const rowsLength = letters[0].length;
+  
+  // use transpose function to flip array, create new array
+  // empty matrix init
+  let newMatrix = [];
 
-module.exports = wordSearch
+  // create an empty array for each column in the original matrix
+  for (let i = 0; i < rowsLength; i++) {
+    newMatrix[i] = [];
+    // add the relative part of the original matrix
+    for (let j = 0; j < columnLength; j++) {
+      newMatrix[i][j] = letters[j][i];
+    }
+
+  }
+
+  const verticalJoin = newMatrix.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+    
+};
+
+module.exports = wordSearch;
